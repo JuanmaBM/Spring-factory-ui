d276ba07c986ee2541c188206d868a2e5d7e6504
jmbarea <jmbarea@atsistemas.com>
2018-05-10 15:40:41 +0200

    Imports the user component into the application configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {HttpModule} from "@angular/http";
+import { HttpModule } from "@angular/http";
 
 import { AppComponent } from './app.component';
 import { DataTableModule } from './components/data-table';
 import { LoginComponent } from './components/login/login.component';
 import { RoleComponent } from "./components/role/role.component";
+import { UserComponent } from "./components/user/user.component";
 
 import { AuthService } from "./services/auth.service";
 import { RoleService } from "./services/role.service";
+import { UserService } from "./services/user.service";
 
-import {UrlPermission} from "./urlPermission/url.permission";
-import {routing} from "./app.routing";
+import { UrlPermission } from "./urlPermission/url.permission";
+import { routing } from "./app.routing";
 
 
 @NgModule({
@@ -20,11 +22,12 @@ import {routing} from "./app.routing";
     AppComponent,
     LoginComponent,
     RoleComponent,
+    UserComponent,
   ],
   imports: [
     BrowserModule,HttpModule,FormsModule,routing,DataTableModule
   ],
-  providers: [AuthService, UrlPermission, RoleService],
+  providers: [AuthService, UrlPermission, RoleService, UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
