30272e9d18f5b29a46ead9b6314f43dc0713b5c8
jmbarea <jmbarea@atsistemas.com>
2018-05-21 19:18:54 +0200

    Adds generic logic to group component

--- src/app/components/group/group.component.ts
+++ src/app/components/group/group.component.ts
@@ -1,7 +1,8 @@
 import { Component, Input, OnInit, NgModule, ViewEncapsulation, ViewChild } from '@angular/core';
-import {RoleService} from "../../services/role.service";
+import { GroupService } from "../../services/group.service";
 import {Router} from "@angular/router";
 import { DataTable, DataTableResource } from '../data-table';
+import { Group } from '../../model/group.model';
 
 
 @Component({
@@ -13,10 +14,58 @@ import { DataTable, DataTableResource } from '../data-table';
 export class GroupComponent implements OnInit {
 
   @ViewChild(DataTable) groupsTable: DataTable;
-  constructor(private router: Router) { }
+  constructor(private groupService: GroupService, private router: Router) { }
+
+  groups = new Array<Group>();
+  group = new Group();
+  tableResource = new DataTableResource(this.groups);
+
+  rowCount = 0;
+  showFormFlag = false;
+  isEditAction = false;
  
   ngOnInit() {
 
   }
+
+  reload(params) {
+    this.getAll();    
+  }
+
+  showForm() {
+    this.group = new Group();
+    this.showFormFlag = true;
+  }
+
+  closeForm() {
+    this.showFormFlag = false;
+    this.isEditAction = false;
+  }
+
+  editRoleAction(roleSelected) {
+    this.group = roleSelected
+    this.showFormFlag = true;
+    this.isEditAction = true;
+  }
+
+  createRole() {
+    this.groupService.post(JSON.stringify(this.group)).subscribe(res => {
+      this.reload(undefined);
+      this.showFormFlag = false;
+    });
+  }
+
+  editRole() {
+    let roleId = this.group.id;
+    let body = JSON.stringify(this.group)
+
+    this.groupService.put(roleId, body).subscribe(_ => this.showFormFlag = false);
+  }
+
+  deleteRole(id: number) {
+    this.groupService.delete(id).subscribe(_ => this.reload(undefined));
+  }
+
+  private getAll = () => this.groupService.get().subscribe(groups => this.groups = groups)
 
 }
