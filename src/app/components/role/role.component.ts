c90fc65b83311a574438f74b365504f97dcb6b4f
jmbarea <jmbarea@atsistemas.com>
2018-05-24 11:10:34 +0200

    Defines an inherit between role and generic component

--- src/app/components/role/role.component.ts
+++ src/app/components/role/role.component.ts
@@ -4,6 +4,8 @@ import {Router} from "@angular/router";
 import {Role} from "../../model/role.model";
 import {PERMISSIONS} from "../../model/permission.mode";
 import { DataTable, DataTableResource } from '../data-table';
+import { GenericComponent } from '../generic.component';
+import { ErrorService } from '../../services/error.service';
 
 
 @Component({
@@ -12,70 +14,28 @@ import { DataTable, DataTableResource } from '../data-table';
   styleUrls: ['./rol.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class RoleComponent implements OnInit {
+export class RoleComponent extends GenericComponent {
 
-  @ViewChild(DataTable) rolesTable: DataTable;
-  constructor(private roleService: RoleService, private router: Router) { }
- 
   permissions = PERMISSIONS;
 
-  role = new Role();
-  roles = new Array<Role>();
-  roleResource = new DataTableResource(this.roles);
-  rolesCount = 0;
-  showRoleForm = false;
-  isEditRoleAction = false;
-
-  ngOnInit() {
-    this.getRoles();
+  constructor(private roleService: RoleService, private errorService: ErrorService) {
+    super();
   }
 
-  reload(params) {
-    this.getRoles();    
+  newModel() {
+    return new Role();
   }
 
-  showForm() {
-    this.role = new Role();
-    this.showRoleForm = true;
+  newModelList() {
+    return new Array<Role>();
   }
 
-  closeForm() {
-    this.showRoleForm = false;
-    this.isEditRoleAction = false;
+  getGenericService() {
+    return this.roleService;
   }
 
-  editRoleAction(roleSelected) {
-    this.role = roleSelected
-    this.showRoleForm = true;
-    this.isEditRoleAction = true;
-  }
-
-  createRole() {
-    this.roleService.post(JSON.stringify(this.role)).subscribe(res => {
-      this.reload(undefined);
-      this.showRoleForm = false;
-    });
-  }
-
-  editRole() {
-    let roleId = this.role.id;
-    let body = JSON.stringify(this.role)
-
-    this.roleService.put(roleId, body).subscribe(_ => this.showRoleForm = false);
-  }
-
-  deleteRole(id: number) {
-    this.roleService.delete(id).subscribe(_ => this.reload(undefined));
-  }
-
-  /**
-   * Retrieves all roles and counts the ocurrences
-   */
-  private getRoles() {
-    this.roleService.get().subscribe(roles => {
-      this.roles = roles
-      this.rolesCount = roles.length
-    });
+  getErrorService() {
+    return this.errorService;
   }
 
 }
