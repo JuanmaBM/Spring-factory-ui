194fb584f281056833e2eaf3ed64fbe57bdf9711
jmbarea <jmbarea@atsistemas.com>
2018-05-24 12:32:26 +0200

    Creates ProductionSchedule model

---
+++ src/app/model/ProductionSchedule.model.ts
@@ -0,0 +1,10 @@
+export class ProductSchedule {
+    name: string;
+    estimatedStartDate: Date;
+    estimatedFinishDate: Date;
+    realStartDate: Date;
+    realFinishDate: Date;
+    state: string;
+}
+
+export const SCHEDULE_STATES: string[] = ["OPEN", "CLOSE", "IN_PROGRESS"];
