af25f0f9362d99dd919445cf234d5912103319a8
jmbarea <jmbarea@atsistemas.com>
2018-06-13 15:55:46 +0200

    Add constructor to Order

--- src/app/model/order.model.ts
+++ src/app/model/order.model.ts
@@ -3,6 +3,15 @@ export class Order {
     name: string;
     description: string;
     measurements: string;
+
+    public constructor(order?: Order) {
+        if (order !== undefined) {
+            this.id = order.id;
+            this.name = order.name;
+            this.description = order.description;
+            this.measurements = order.measurements;
+        }
+    }
 }
 
 export const MEASUREMENTS: string[] = [
